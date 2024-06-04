d765709dbfed646128b9531029b7287f3c99f8a6
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-06-04 22:55:55 +0300

    Create project, add dependencies

---
+++ src/main/java/by/artsem/druzhbahub/DruzhbaHubAppApplication.java
@@ -0,0 +1,13 @@
+package by.artsem.druzhbahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruzhbaHubAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruzhbaHubAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/artsem/druzhbahub/DruzhbaHubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.artsem.druzhbahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DruzhbaHubAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
