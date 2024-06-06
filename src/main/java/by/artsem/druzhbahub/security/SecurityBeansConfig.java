210f21f5408ddf53d54633393dac00931ee2ac13
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-06-06 11:06:40 +0300

    Enable spring security

---
+++ src/main/java/by/artsem/druzhbahub/security/SecurityBeansConfig.java
@@ -0,0 +1,16 @@
+package by.artsem.druzhbahub.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityBeansConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
