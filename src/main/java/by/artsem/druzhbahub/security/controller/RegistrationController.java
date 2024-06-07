c7a8b2ed9dd4e0f41b9ca53eba8915a6c4324d67
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-06-07 15:33:05 +0300

    Refactor RegistrationController

--- src/main/java/by/artsem/druzhbahub/security/controller/RegistrationController.java
+++ src/main/java/by/artsem/druzhbahub/security/controller/RegistrationController.java
@@ -8,10 +8,12 @@ import by.artsem.druzhbahub.security.service.ConfirmationTokenService;
 import by.artsem.druzhbahub.security.service.EmailService;
 import by.artsem.druzhbahub.security.service.EmailTextService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/register")
+@RequestMapping("/api/v1/register")
 @RequiredArgsConstructor
 public class RegistrationController {
 
@@ -24,20 +26,21 @@ public class RegistrationController {
     private final EmailService emailService;
 
     @PostMapping
-    public void register(@RequestBody RegistrationRequestDTO dto) {
+    public ResponseEntity<HttpStatus> register(@RequestBody RegistrationRequestDTO dto) {
         Account account = accountService.createAccountAndProfile(dto);
-        ConfirmationToken token = confirmationTokenService.createToken(account);
+        ConfirmationToken token = confirmationTokenService.create(account);
         emailTextService.createEmailMessage(token.getToken());
         emailService.sendEmail(
                 account.getEmail(),
                 emailTextService.getSubject(),
                 emailTextService.getBody()
         );
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/confirm")
-    public String confirm(@RequestParam("token") String token) {
+    public ResponseEntity<HttpStatus> confirm(@RequestParam("token") String token) {
         accountService.confirmEmail(token);
-        return "Email confirmed successfully";
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
