7220f2d97de9407f4241315d7cff7716b0776c14
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-06-11 11:12:11 +0300

    Refactor service interface

--- src/main/java/by/artsem/druzhbahub/service/CRUDService.java
+++ src/main/java/by/artsem/druzhbahub/service/CRUDService.java
@@ -1,12 +1,15 @@
 package by.artsem.druzhbahub.service;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CRUDService<T, ID> {
     T create(T entity);
-    Optional<T> getById(ID id);
+
+    T getById(ID id);
+
     List<T> getAll();
+
     T update(ID id, T entity);
+
     void delete(ID id);
 }
