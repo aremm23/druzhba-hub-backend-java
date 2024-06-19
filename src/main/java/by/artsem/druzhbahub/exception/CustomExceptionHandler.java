b49079c8f7014623639dfa6a4e9587fbf677fd15
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-06-19 19:18:24 +0300

    fix up warnings

--- src/main/java/by/artsem/druzhbahub/exception/CustomExceptionHandler.java
+++ src/main/java/by/artsem/druzhbahub/exception/CustomExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.io.IOException;
 import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
 
--- src/main/java/by/artsem/druzhbahub/model/Image.java
+++ src/main/java/by/artsem/druzhbahub/model/Image.java
@@ -1,9 +1,7 @@
 package by.artsem.druzhbahub.model;
 
-import java.time.LocalDateTime;
-
 public interface Image {
     Long getId();
+
     String getGcsFileName();
-    LocalDateTime getUploadTime();
 }
