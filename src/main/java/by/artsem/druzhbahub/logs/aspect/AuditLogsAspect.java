9a0a983e1cf4a423fe15be4683b3ff6115b33a09
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-06-25 12:02:49 +0300

    Refactor logging aspects

---
+++ src/main/java/by/artsem/druzhbahub/logs/aspect/AuditLogsAspect.java
@@ -0,0 +1,81 @@
+package by.artsem.druzhbahub.logs.aspect;
+
+import by.artsem.druzhbahub.logs.LogParameters;
+import by.artsem.druzhbahub.logs.LoggingLevels;
+import by.artsem.druzhbahub.logs.MongoAppLogger;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@RequiredArgsConstructor
+public class AuditLogsAspect {
+
+    private final MongoAppLogger mongoAppLogger;
+
+    @Pointcut("execution(* by.artsem.druzhbahub.security.controller.RegistrationController.register(..))")
+    public void registerMethod() {
+    }
+
+    @Pointcut("execution(* by.artsem.druzhbahub.security.service.EmailService.sendEmail(..))")
+    public void sendEmailMethod() {
+    }
+
+    @Pointcut("execution(* by.artsem.druzhbahub.security.service.AccountService.updatePassword(..))")
+    public void updatePasswordMethod() {
+    }
+
+    @Pointcut("execution(* by.artsem.druzhbahub.security.service.AccountService.updateEmail(..))")
+    public void updateEmailMethod() {
+    }
+
+    @Pointcut("execution(* by.artsem.druzhbahub.security.service.AccountService.confirmEmail(..))")
+    public void confirmEmailWithToken() {
+    }
+
+    @AfterReturning("registerMethod()")
+    public void logAfterRegister(JoinPoint joinPoint) {
+        logAction(joinPoint, "User registered");
+    }
+
+    @AfterReturning("confirmEmailWithToken()")
+    public void logAfterConfirming(JoinPoint joinPoint) {
+        logAction(joinPoint, "User email confirmed");
+    }
+
+    @AfterReturning("sendEmailMethod()")
+    public void logAfterSendEmail(JoinPoint joinPoint) {
+        logAction(joinPoint, "Email sent");
+    }
+
+    @AfterReturning("updatePasswordMethod()")
+    public void logAfterUpdatePassword(JoinPoint joinPoint) {
+        logAction(joinPoint, "Password updated");
+    }
+
+    @AfterReturning("updateEmailMethod()")
+    public void logAfterUpdateEmail(JoinPoint joinPoint) {
+        logAction(joinPoint, "Email updated");
+    }
+
+    private void logAction(JoinPoint joinPoint, String action) {
+        //TODO: get user id
+        String userId = "0";
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String message = String.format("Action: %s, Method: %s, Class: %s, User: %s", action, methodName, className, userId);
+
+        mongoAppLogger.log(
+                LogParameters.builder()
+                        .userId(userId)
+                        .level("AUDIT")
+                        .type(LoggingLevels.AUDIT)
+                        .message(message)
+                        .build()
+        );
+    }
+}
--- src/main/java/by/artsem/druzhbahub/logs/aspect/LoggingAspect.java
+++ src/main/java/by/artsem/druzhbahub/logs/aspect/LoggingAspect.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
     private final MongoAppLogger mongoAppLogger;
 
-    @Before("execution(* by.artsem.druzhbahub.service.*.*(..))")
+    /*@Before("execution(* by.artsem.druzhbahub.service.*.*(..))")
     public void logBeforeMethod(JoinPoint joinPoint) {
         LogParameters parameters = LogParameters.builder()
                 .type(LoggingLevels.AUDIT)
@@ -26,13 +26,13 @@ public class LoggingAspect {
                 .build();
 
         mongoAppLogger.log(parameters);
-    }
+    }*/
 
     @AfterThrowing(pointcut = "execution(* by.artsem.druzhbahub.service.*.*(..))", throwing = "exception")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
         LogParameters parameters = LogParameters.builder()
                 .type(LoggingLevels.EXCEPTION)
-                .level("ERROR")
+                .level("WARNING")
                 .message("Exception in method: " + joinPoint.getSignature() + " with message: " + exception.getMessage())
                 .userId(getCurrentUserId())
                 .build();
