63d18061e53845b9f2bd8c55d2844310d98da6e7
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-07-03 10:51:05 +0300

    Modify configuration for jwt authentication

--- src/main/java/by/artsem/druzhbahub/security/SecurityBeansConfig.java
+++ src/main/java/by/artsem/druzhbahub/security/SecurityBeansConfig.java
@@ -1,14 +1,43 @@
 package by.artsem.druzhbahub.security;
 
+import by.artsem.druzhbahub.exception.DataNotFoundedException;
+import by.artsem.druzhbahub.security.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityBeansConfig {
+    private final AccountRepository accountRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email ->
+                accountRepository.findByEmail(email)
+                        .orElseThrow(() -> new DataNotFoundedException("Account not founded"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
--- src/main/java/by/artsem/druzhbahub/security/SecurityConfig.java
+++ src/main/java/by/artsem/druzhbahub/security/SecurityConfig.java
@@ -1,27 +1,58 @@
 package by.artsem.druzhbahub.security;
 
+import by.artsem.druzhbahub.security.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class SecurityConfig {
+    private static final String[] WHITE_LIST_URL = {
+            "/api/security/**"
+    };
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    private final AuthenticationProvider authenticationProvider;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()
-                );
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST_URL)
+                                .permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/api/**", "GET"))
+                                .hasAnyRole("ADMIN", "USER")
+                                .requestMatchers(new AntPathRequestMatcher("/api/**", "PUT"))
+                                .hasRole("ADMIN")
+                                .requestMatchers(new AntPathRequestMatcher("/api/**", "PATCH"))
+                                .hasRole("ADMIN")
+                                .requestMatchers(new AntPathRequestMatcher("/api/**", "DELETE"))
+                                .hasRole("ADMIN")
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
 
         return http.build();
     }
+
 }
+
