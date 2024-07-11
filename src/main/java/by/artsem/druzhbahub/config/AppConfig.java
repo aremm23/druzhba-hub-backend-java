3d81af3261300f0fce3fd53facd4c83af9f672d8
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-07-11 11:53:25 +0300

    Unite configuration in one class AppConfig

--- src/main/java/by/artsem/druzhbahub/config/AppConfig.java
+++ src/main/java/by/artsem/druzhbahub/config/AppConfig.java
@@ -3,15 +3,17 @@ package by.artsem.druzhbahub.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
-public class GoogleCloudStorageConfig {
+public class AppConfig {
     @Value("${gcp.project-id}")
     private String projectId;
 
@@ -26,4 +28,16 @@ public class GoogleCloudStorageConfig {
                 .setCredentials(credentials)
                 .build().getService();
     }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
 }
